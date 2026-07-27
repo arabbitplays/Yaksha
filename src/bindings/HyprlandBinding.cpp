6eba71d91258985add3bdbba2c03d9bc85cad6db
Oschdi <osterjakob@gmail.com>
2026-07-27 23:37:20 +0200

    Workspace switch fix

--- src/bindings/HyprlandBinding.cpp
+++ src/bindings/HyprlandBinding.cpp
@@ -37,8 +37,8 @@ void HyprlandBinding::focusAndOpenWorkspaces(
     const std::optional<std::string>& final_focus_monitor) const {
     std::string batch = "hyprctl --batch \"";
     for (size_t i = 0; i < focuses.size(); ++i) {
-        batch += "dispatch focusmonitor " + focuses[i].monitor_name + " ; ";
-        batch += "dispatch workspace " + std::to_string(focuses[i].hyprland_workspace_id);
+        std::string id_string = std::to_string(focuses[i].hyprland_workspace_id);
+        batch += std::format("dispatch focusmonitor {} ; dispatch focusworkspaceoncurrentmonitor {}", focuses[i].monitor_name, id_string);
         const bool needs_separator = (i + 1 < focuses.size()) || final_focus_monitor.has_value();
         if (needs_separator) {
             batch += " ; ";
